8a08be4ad770bf84913d8b9df74cb63120993c9b
Ichinichi <PechenkinPavelV@yandex.ru>
2024-12-24 17:05:19 +0300

    Add test cases

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Global Constants
 BASE_URL = "https://abc07.ru"
 
 CATALOG_URL = BASE_URL + "/catalog/kraski_i_lak/"
+SEARCH_URL = BASE_URL + "/search/?q=%D0%BB%D0%B0%D0%BA"
 MAIN_PAGE_URL = BASE_URL
 FAVORITES_PAGE_URL = BASE_URL + "/favorite/"
 PRODUCT_ROW_SELECTOR = ".product-row .js-item"
@@ -65,18 +65,22 @@ def click_favorites_button(product):
     fav_icon = product.find_element(By.CSS_SELECTOR, FAV_ICON_SELECTOR)
     fav_icon.click()
 
+    time.sleep(3)
 
 def open_favorites(driver):
     driver.get(FAVORITES_PAGE_URL)
 
 def is_product_in_favorites(driver, title, price):
     products = driver.find_elements(By.CSS_SELECTOR, PRODUCT_ROW_SELECTOR)
+
+    found = 0
     for p in products:
         product_title = p.find_element(By.CSS_SELECTOR, PRODUCT_TITLE_SELECTOR).text
         product_price = clean_price(p.find_element(By.CSS_SELECTOR, PRODUCT_PRICE_SELECTOR).text)
         if title.lower() == product_title.lower() and price == product_price:
-            return True
-    return False
+            found += 1
+
+    return found == 1
 
 def get_product_info_from_product_page(driver, product_link):
     driver.get(product_link)
@@ -84,13 +88,11 @@ def get_product_info_from_product_page(driver, product_link):
     price = clean_price(driver.find_element(By.CSS_SELECTOR, PRODUCT_PAGE_PRICE_SELECTOR).text)
     return title, price
 
-def add_to_favorites_from_product_page(driver):
+def click_favorites_button_product_page(driver):
     fav_icon = driver.find_element(By.CSS_SELECTOR, PRODUCT_PAGE_FAV_ICON_SELECTOR)
     fav_icon.click()
 
-def remove_from_favorites_from_product_page(driver):
-    fav_icon = driver.find_element(By.CSS_SELECTOR, PRODUCT_PAGE_FAV_ICON_SELECTOR)
-    fav_icon.click()
+    time.sleep(3)
 
 # Tests
 @pytest.mark.parametrize("test_name", ["Добавление товара в избранное из каталога"])
@@ -113,11 +115,23 @@ def test_add_from_product_page(driver, test_name):
 
     # Act
     product_page_title, product_page_price = get_product_info_from_product_page(driver, product_link)
-    add_to_favorites_from_product_page(driver)
+    click_favorites_button_product_page(driver)
     open_favorites(driver)
 
     # Assert
     assert is_product_in_favorites(driver, product_page_title, product_page_price)
+
+@pytest.mark.parametrize("test_name", ["Добавление товара в избранное со страницы поиска"])
+def test_add_from_search(driver, test_name):
+    # Arrange
+    product, title, price = get_first_product_info_catalog(driver, SEARCH_URL)
+
+    # Act
+    click_favorites_button(product)
+    open_favorites(driver)
+
+    # Assert
+    assert is_product_in_favorites(driver, title, price)
 
 @pytest.mark.parametrize("test_name", ["Добавление товара в избранное с главной страницы"])
 def test_add_from_main_page(driver, test_name):
@@ -157,11 +171,9 @@ def test_remove_from_catalog(driver, test_name):
     # Arrange
     product, title, price = get_first_product_info_catalog(driver, CATALOG_URL)
     click_favorites_button(product)
-    time.sleep(3)
 
     # Act
     click_favorites_button(product)
-    time.sleep(3)
 
     open_favorites(driver)
 
@@ -174,28 +186,38 @@ def test_remove_from_product_page(driver, test_name):
     product, catalog_title, catalog_price = get_first_product_info_catalog(driver, CATALOG_URL)
     product_link = product.find_element(By.CSS_SELECTOR, PRODUCT_TITLE_SELECTOR).get_attribute("href")
     product_page_title, product_page_price = get_product_info_from_product_page(driver, product_link)
-    add_to_favorites_from_product_page(driver)
-    time.sleep(3)
+    click_favorites_button_product_page(driver)
 
     # Act
-    remove_from_favorites_from_product_page(driver)
-    time.sleep(3)
+    click_favorites_button_product_page(driver)
 
     open_favorites(driver)
 
     # Assert
     assert not is_product_in_favorites(driver, product_page_title, product_page_price)
 
+@pytest.mark.parametrize("test_name", ["Удаление товара из избранного со страницы поиска"])
+def test_remove_from_catalog(driver, test_name):
+    # Arrange
+    product, title, price = get_first_product_info_catalog(driver, SEARCH_URL)
+    click_favorites_button(product)
+
+    # Act
+    click_favorites_button(product)
+
+    open_favorites(driver)
+
+    # Assert
+    assert not is_product_in_favorites(driver, title, price)
+
 @pytest.mark.parametrize("test_name", ["Удаление товара из избранного с главной страницы"])
 def test_remove_from_main_page(driver, test_name):
     # Arrange
     product, title, price = get_first_product_info_main_page(driver, MAIN_PAGE_URL)
     click_favorites_button(product)
-    time.sleep(3)
 
     # Act
     click_favorites_button(product)
-    time.sleep(3)
 
     open_favorites(driver)
 
@@ -207,14 +229,12 @@ def test_remove_from_favorites_page(driver, test_name):
     # Arrange
     product, title, price = get_first_product_info_catalog(driver, CATALOG_URL)
     click_favorites_button(product)
-    time.sleep(3)
 
     open_favorites(driver)
 
     # Act
     item = driver.find_element(By.CSS_SELECTOR, FAVORITE_PAGE_ITEM_SELECTOR)
     click_favorites_button(item)
-    time.sleep(3)
 
     # Assert
     assert not is_product_in_favorites(driver, title, price)
